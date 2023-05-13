9f7cd7882f736013815d0645c9939977ad91899a
Robin Dong <rdong@woolworths.com.au>
2023-05-13 20:26:37 +1000

    Refactor code to functions

--- bot_service.py
+++ bot_service.py
@@ -1,8 +1,12 @@
 # This example requires the 'message_content' intent.
 import os
+import cv2
 import asyncio
 import discord
 import requests
+import numpy as np
+
+from io import BytesIO
 
 
 intents = discord.Intents.default()
@@ -14,34 +18,66 @@ DONGNIAO_API_KEY = os.getenv("DONGNIAO_API_KEY")
 DONGNIAO_API_URL = "http://ca.dongniao.net/dongniao_2"
 
 
-async def dongniao_api(message):
-    url = message.attachments[0].url
+def dongniao_result_id(url):
     values = {"image": "image", "api_key": DONGNIAO_API_KEY, "url": url, "async": 1}
     resp = requests.post(DONGNIAO_API_URL, data=values)
-    print("[url]", url)
     reply = resp.json()
     print("<<< ", reply, " >>>")
     if reply[0] != 1000:
-        return
+        return None
     result_id = reply[1]
-    values = {
-        "resultidv3": result_id,
-        "api_key": DONGNIAO_API_KEY,
-    }
+    return result_id
+
+
+async def dongniao_box_list(result_id):
+    values = {"resultidv3": result_id, "api_key": DONGNIAO_API_KEY}
     retry = 0
+    cat_list = None
     while retry < 10:
-        await asyncio.sleep(1)
+        await asyncio.sleep(2)
         resp = requests.post(DONGNIAO_API_URL, data=values)
         reply = resp.json()
         print("((( ", reply, " )))")
         if len(reply) > 0 and reply[0] != 1000:
+            if type(reply[0]) == dict:
+                cat_list = reply
             break
         retry += 1
-    reply = discord.Embed(title="Looks good!", description=message.author.mention)
-    reply.set_image(url=url)
+    return cat_list
+
+
+def draw_image(url, box):
+    # get image
+    resp = requests.get(url, stream=True)
+    if resp.status_code != 200:
+        return None, None
+    buffer = BytesIO(resp.content)
+    file_bytes = np.asarray(bytearray(buffer.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
+    cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (10, 10, 255), 2)
+    is_success, buffer = cv2.imencode(".jpg", img)
+    return is_success, buffer
+
+
+async def dongniao_api(message):
+    url = message.attachments[0].url
+    result_id = dongniao_result_id(url)
+    if not result_id:
+        return
+    cat_list = await dongniao_box_list(result_id)
+    if not cat_list:
+        return
     # if message.channel.type == discord.ChannelType.text:
+    for item in cat_list:
+        box, lst = item["box"], item["list"]
+        em = discord.Embed(title=f"{lst[0][1]}", description=message.author.mention)
+        is_success, buffer = draw_image(url, box)
+        if is_success:
+            file = discord.File(filename=f"{lst[0][1]}.jpg", fp=BytesIO(buffer))
+            em.set_image(url=f"attachment://{lst[0][1]}.jpg")
+            await message.channel.send(embed=em, file=file)
     print("Done")
-    await message.channel.send(file=discord.File("/Users/rdong/Downloads/vicuna.png"))
+    # await message.channel.send(file=file)
 
 
 @client.event
